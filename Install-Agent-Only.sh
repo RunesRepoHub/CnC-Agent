b17448bd4f6f7cfc6572149b8f07d47264cfef9c
rune <christian.rune1998@gmail.com>
2023-10-17 12:33:35 +0200

    added a only agent install script

---
+++ Install-Agent-Only.sh
@@ -0,0 +1,26 @@
+FILE=~/Install-Agent
+if [ -d "$FILE" ]; then
+    ## Clear screen for better overview
+    
+    ## Inform the user if the file has already been downloaded
+    echo "$FILE exists."
+    echo "Do you want to delete the old files and install a new version?"
+    echo
+    ## Ask the user for action input
+    read -p "Are you sure? " -n 1 -r
+    ## Move to a new line
+    echo
+    ## Check user input    
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        ## Remove old files
+        rm -r ~/Install-Agent
+        ## Clones new files
+        echo
+        echo
+        git clone --branch Dev https://github.com/RunesRepoHub/CnC-Agent.git;
+        ## Runs the installation script
+        bash ~/CnC-Agent/Install-Agent.sh;
+else
+        bash ~/CnC-Agent/Install-Agent.sh;
+fi
