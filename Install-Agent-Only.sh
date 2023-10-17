083649be10bc619dcfcfbf541bb1f7c00f501b9f
rune <christian.rune1998@gmail.com>
2023-10-17 17:27:18 +0200

    Hotfix

--- Install-Agent-Only.sh
+++ Install-Agent-Only.sh
@@ -39,7 +39,7 @@ if [ -d "$FILE" ]; then
         echo
         git clone --branch Dev https://github.com/RunesRepoHub/CnC-Agent.git;
         ## Runs the installation script
-        wget -O ~/CnC-Agent/config.sh "$get_config_url" > /dev/null 2>&1;
+        wget -O ~/CnC-Agent/config.sh https://raw.githubusercontent.com/RunesRepoHub/CnC-WebGUI/Dev/config.sh > /dev/null 2>&1;
         bash ~/CnC-Agent/Install-Agent.sh;
     else
         bash ~/CnC-Agent/Install-Agent.sh;
