b70054794acb6be1e91c38c5552c23843ff133a8
rune <christian.rune1998@gmail.com>
2023-10-19 13:06:44 +0200

    Testing new code for packages

--- Debian-Scripts/Packages.sh
+++ Debian-Scripts/Packages.sh
@@ -3,53 +3,49 @@
 # Source the configuration script
 source ~/CnC-Agent/config.sh
 
-databaseip=$(cat "$dbip")
 me=$(basename "$0")
 
-# Define your REST API endpoints for reading, creating, and updating data
-API_READ_ENDPOINT="http://$databaseip:3000/read/packages"
-API_CREATE_ENDPOINT="http://$databaseip:3000/create/packages"
-API_UPDATE_ENDPOINT="http://$databaseip:3000/update/packages"
-
-# Function to retrieve all installed packages
-get_installed_packages() {
-    dpkg -l | awk '/^ii/ {print $2}'
-}
-
-# Retrieve the database data for the specified hostname
-get_database_packages() {
-    local hostname="$1"
-    curl -s "$API_READ_ENDPOINT/$hostname"
-}
-
-# Function to check if a package is installed and return "Installed" or "Not Installed"
-check_installed() {
-    local package_name="$1"
-    dpkg -l | grep -q "^ii $package_name " && echo "Installed" || echo "Not Installed"
-}
-
-# Check if there are installed packages
-installed_packages=$(get_installed_packages)
-if [ -z "$installed_packages" ]; then
-    echo "No installed packages found. Exiting."
-    exit 0
-fi
-
-# Compare the collected data with the database data
-hostname="$HOSTNAME"
-db_packages_json=$(get_database_packages "$hostname")
-
-for package_name in $installed_packages; do
-    # Check if the package is in the database
-    if echo "$db_packages_json" | jq -e '.[] | select(.hostname == "'"${hostname}"'" and .packagename == "'"${package_name}"'")' >/dev/null; then
-        echo "Data for package $package_name is up to date."
+# Function to check if a package is installed
+package_installed() {
+    local package_name=$1
+    if dpkg -l | grep -q "ii  $package_name "; then
+        echo "Installed"
     else
-        # Data doesn't exist, so create a new entry with "Installed" or "Not Installed" status
-        status=$(check_installed "$package_name")
-        data='{"hostname":"'"$hostname"'","packagename":"'"$package_name"'","installed":"'"$status"'"}'
-        response=$(curl -X POST -H "Content-Type: application/json" -d "$data" "$API_CREATE_ENDPOINT")
-        echo "Data inserted from $me for package $package_name. Status: $status"
+        echo "Not Installed"
     fi
+}
+
+# Define your hostname
+hostname="$HOSTNAME"
+
+# Function to send an HTTP POST request to create a package record
+send_create_request() {
+    local data="$1"
+    curl -X POST -H "Content-Type: application/json" -d "$data" "$API_CREATE_ENDPOINT"
+}
+
+# Function to send an HTTP PUT request to update a package record
+send_update_request() {
+    local hostname="$1"
+    local package_name="$2"
+    local data="$3"
+    curl -X PUT -H "Content-Type: application/json" -d "$data" "$API_UPDATE_ENDPOINT/$hostname/$package_name"
+}
+
+# Retrieve the list of installed packages
+installed_packages=$(dpkg -l | awk '/^ii/ {print $2}')
+
+# Loop through the installed packages and update the database via the API
+for package_name in $installed_packages; do
+    package_status=$(package_installed "$package_name")
+    data="{\"hostname\":\"$hostname\",\"packagename\":\"$package_name\",\"status\":\"$package_status\"}"
+    
+    # Check if the package is in the database
+    response=$(send_create_request "$data")
+    echo "Data inserted/updated from $me for package $package_name."
 done
+
+# Send a request to the API to check for removed packages and update their status
+curl -X PUT "$API_UPDATE_ENDPOINT/$hostname"
 
 echo "All package data is up to date."
