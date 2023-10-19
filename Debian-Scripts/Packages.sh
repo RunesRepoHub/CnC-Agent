5e6bf481a65e60d43d096e2e49671c50286f4fe3
rune <christian.rune1998@gmail.com>
2023-10-19 12:11:24 +0200

    Testing new package code

--- Debian-Scripts/Packages.sh
+++ Debian-Scripts/Packages.sh
@@ -16,44 +16,39 @@ get_installed_packages() {
     dpkg -l | awk '/^ii/ {print $2, $3}'
 }
 
-# Function to retrieve the database data for the specified hostname
+# Retrieve the database data for the specified hostname
 get_database_packages() {
     local hostname="$1"
-    local db_data=$(curl -s "$API_READ_ENDPOINT/$hostname")
-    if [ -z "$db_data" ]; then
-        echo "[]"
-    else
-        echo "$db_data" | jq -c '.[0].packages'
-    fi
+    curl -s "$API_READ_ENDPOINT/$hostname"
 }
 
-# Compare the collected data with the database data for each package
+# Check if there are installed packages
 installed_packages=$(get_installed_packages)
+if [ -z "$installed_packages" ]; then
+    echo "No installed packages found. Exiting."
+    exit 0
+fi
+
+# Compare the collected data with the database data
+hostname="$HOSTNAME"
+db_packages_json=$(get_database_packages "$hostname")
+
 for package_info in $installed_packages; do
     package_name=$(echo "$package_info" | awk '{print $1}')
     package_version=$(echo "$package_info" | awk '{print $2}')
 
-    db_packages_json=$(get_database_packages "$HOSTNAME")
-    
-    if ! echo "$db_packages_json" | grep -q "\"packagename\": \"$package_name\", \"packageversion\": \"$package_version\""; then
-        # Data is different, so update or insert it
-        DATA=$(cat <<EOF
-        {
-            "hostname": "$HOSTNAME",
-            "packagename": "$package_name",
-            "packageversion": "$package_version"
-        }
-EOF
-)
+    data='{"hostname":"'"$hostname"'","package":"'"$package_name $package_version"'"}'
+
+    if ! echo "$db_packages_json" | grep -q "$package_name $package_version"; then
         if [ -z "$db_packages_json" ] || [ "$db_packages_json" == "null" ]; then
             # Data doesn't exist, so create a new entry
-            response=$(curl -X POST -H "Content-Type: application/json" -d "$DATA" "$API_CREATE_ENDPOINT" >/dev/null 2>&1)
+            response=$(curl -X POST -H "Content-Type: application/json" -d "$data" "$API_CREATE_ENDPOINT")
             echo "Data inserted from $me."
-        else {
+        else
             # Data exists, so update it
-            response=$(curl -X PUT -H "Content-Type: application/json" -d "$DATA" "$API_UPDATE_ENDPOINT/$HOSTNAME" >/dev/null 2>&1)
+            response=$(curl -X PUT -H "Content-Type: application/json" -d "$data" "$API_UPDATE_ENDPOINT/$hostname")
             echo "Data updated from $me."
-        }
+        fi
     fi
 done
 
